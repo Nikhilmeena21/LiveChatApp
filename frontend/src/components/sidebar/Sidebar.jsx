c8e240c9bed24e0085f263e807f1f50914e9a39f
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2024-07-24 01:19:02 +0530

    Update Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -3,18 +3,21 @@ import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
-    return (
-        <div className='w-1/3 bg-gray-50 border-r border-gray-200 flex flex-col'>
-            <div className='p-4'>
-                <SearchInput />
-            </div>
-            <div className='flex-1 overflow-y-auto'>
-                <Conversations />
-            </div>
-            <div className='p-4 border-t border-gray-200'>
-                <LogoutButton />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="w-1/3 bg-gray-800 text-white border-r border-gray-700 flex flex-col" // Updated classes
+    >
+      <div className="p-4">
+        <SearchInput />
+      </div>
+      <div className="flex-1 overflow-y-auto">
+        <Conversations />
+      </div>
+      <div className="p-4 border-t border-gray-700">
+        <LogoutButton />
+      </div>
+    </div>
+  );
 };
+
 export default Sidebar;
