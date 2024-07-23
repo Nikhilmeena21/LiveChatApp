e76f6cd5a0ea1f7d33639ea832b1c15220137058
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2024-07-24 01:04:22 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://livechatapp-ax7x.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
