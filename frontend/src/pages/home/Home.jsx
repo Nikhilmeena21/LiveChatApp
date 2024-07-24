28c8b040fc9713be40cb4ec63e7ca7cd11946193
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2024-07-24 10:57:40 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
+
 const Home = () => {
     return (
-        <div className='flex h-screen bg-gray-100'>
-            <div className='flex w-full max-w-6xl mx-auto my-4 rounded-xl overflow-hidden shadow-2xl bg-white'>
-                <Sidebar />
-                <MessageContainer />
+        <div className='flex h-screen w-full bg-gray-900 p-2'>
+            <div className='flex w-full max-w-7xl mx-auto rounded-lg overflow-hidden'>
+                <div className="w-[400px] bg-gray-800">
+                    <Sidebar />
+                </div>
+                <div className="flex-1 bg-gray-700">
+                    <MessageContainer />
+                </div>
             </div>
         </div>
     );
 };
+
 export default Home;
