22a03a2aeba68671417027f82f37da3fce2be352
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2024-07-24 10:37:41 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,10 +3,14 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
     return (
-        <div className='flex h-screen w-full max-w-6xl mx-auto'>
-            <div className='flex w-full rounded-xl overflow-hidden shadow-2xl bg-gray-800'>
-                <Sidebar />
-                <MessageContainer />
+        <div className='flex h-screen w-full max-w-7xl mx-auto'>
+            <div className='flex w-full rounded-xl overflow-hidden bg-gray-800'>
+                <div className="w-1/3 max-w-sm">
+                    <Sidebar />
+                </div>
+                <div className="flex-1">
+                    <MessageContainer />
+                </div>
             </div>
         </div>
     );
