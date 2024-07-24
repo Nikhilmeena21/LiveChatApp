cdabb5c2402fe5c15d4782539a2546c506bd8b56
Nikhilmeena21 <Nikhil__Meena@outlook.com>
2024-07-24 10:39:56 +0530

    Update MessageContainer.jsx

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -7,21 +7,19 @@ import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
-
+  
   useEffect(() => {
-    // cleanup function (unmounts)
     return () => setSelectedConversation(null);
   }, [setSelectedConversation]);
 
   return (
-    <div className="md:min-w-[450px] flex flex-col bg-gray-800">
+    <div className="flex flex-col h-full">
       {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
-          {/* Header */}
           <div className="bg-gray-700 px-4 py-2 mb-2 text-white">
-            <span className="label-text">To:</span>
+            <span className="label-text">To:</span>{" "}
             <span className="font-bold">{selectedConversation.fullName}</span>
           </div>
           <Messages />
@@ -39,7 +37,7 @@ const NoChatSelected = () => {
   return (
     <div className="flex items-center justify-center w-full h-full bg-gray-800 text-white">
       <div className="px-4 text-center sm:text-lg md:text-xl font-semibold flex flex-col items-center gap-2">
-        <p>Welcome  {authUser.fullName} ❄</p>
+        <p>Welcome {authUser.fullName} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
